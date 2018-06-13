f983e6c1df740f9a7ecf0fc67859623c55583d1b
starbucks2 <ratsovphilip@gmail.com>
2018-06-13 18:17:10 +0300

    Add files via upload

---
+++ 01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().ToArray();
+
+            Dictionary<string, string> phoneBook = new Dictionary<string, string>();
+
+            while (input[0] != "END")
+            {
+                if (input[0] == "A")
+                {
+
+                    phoneBook.Add(input[1], input[2]);
+
+                }
+                else if (input[0] == "S")
+                {
+                    if (phoneBook.ContainsKey(input[1]) == false)
+                    {
+                        Console.WriteLine($"Contact {input[1]} does not exist.");
+                    }
+                    else
+                    {
+                   // Console.WriteLine($"{phoneBook.Key} -> {result.Value}");
+
+                    }
+
+
+                }
+
+            input = Console.ReadLine().Split().ToArray();
+            }
+
+        }
+    }
+}
+
---
+++ 02/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().ToArray();
+
+            SortedDictionary<string, string> phoneBook = new SortedDictionary<string, string>();
+
+            List<string> result = new List<string>();
+
+            while (input[0] != "END")
+            {
+                if (input[0] == "A")
+                {
+                    if (phoneBook.ContainsKey(input[1]))
+                    {
+                        string name = input[1];
+                        string phone = input[2];
+                        phoneBook[name] = phone;
+                    }
+                    else
+                    {
+                        phoneBook.Add(input[1], input[2]);
+                    }
+                }
+                else if (input[0] == "ListAll")
+                {
+                    foreach (var item in phoneBook)
+                    {
+                        Console.WriteLine($"{item.Key} -> {item.Value}");
+                    }
+
+
+                }
+                else if (input[0] == "S")
+                {
+                    if (phoneBook.ContainsKey(input[1]) == false)
+                    {
+                        Console.WriteLine($"Contact {input[1]} does not exist.");
+                    }
+                    else
+                    {
+
+                        string foundName = input[1];
+                        string foundNumber = phoneBook[foundName];
+
+                        Console.WriteLine($"{foundName} -> {foundNumber}");
+
+
+                    }
+
+                }
+                input = Console.ReadLine().Split().ToArray();
+
+            }
+        }
+    }
+}
